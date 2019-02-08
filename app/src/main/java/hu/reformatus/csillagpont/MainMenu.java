f0417b74ecb9009f61bd2fa54881649d91615f09
Bunta Roland <buntaroland@gmail.com>
2019-02-08 19:18:55 +0100

    Add template main menu and plain activities
    An example main menu page is added with four additional pages.
    Template options select and floating action button also added.

---
+++ app/src/main/java/hu/reformatus/csillagpont/MainMenu.java
@@ -0,0 +1,114 @@
+package hu.reformatus.csillagpont;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getString(R.string.app_name));
+
+        final Button mapButton = findViewById(R.id.main_menu_map_button);
+        final Button programsButton = findViewById(R.id.main_menu_programs_button);
+        final Button newsButton = findViewById(R.id.main_menu_news_button);
+        final Button favouritesButton = findViewById(R.id.main_menu_favourites_button);
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        programsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProgramsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        newsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NewsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        favouritesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FavouritesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton exampleFloatingButton = findViewById(R.id.fab);
+        exampleFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, getString(R.string.main_menu_options_default_button), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.options_settings:
+                showSettings();
+                return true;
+            case R.id.options_logout:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void showSettings(){
+
+        Context context = getApplicationContext();
+        CharSequence text = getString(R.string.main_menu_options_settings);
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+
+    public void logout(){
+
+        Context context = getApplicationContext();
+        CharSequence text = getString(R.string.main_menu_options_logout);
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+
+}
