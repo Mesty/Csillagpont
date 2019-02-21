aa38cf7eb323dd88f062cd406c5ce6e76df9456b
Zoltan Antal <zoltan.antal.11@gmail.com>
2019-02-21 17:52:41 +0100

    Added Calendar view in Programs menu

---
+++ app/src/main/java/hu/reformatus/csillagpont/model/programs/databases/Database.java
@@ -0,0 +1,13 @@
+package hu.reformatus.csillagpont.model.programs.databases;
+
+import android.content.Context;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class Database extends SQLiteAssetHelper {
+    private static final String DATABASE_NAMES = "events.db";
+    private static final int DATABASE_VERSION = 3;
+    public Database(Context context) {
+        super(context, DATABASE_NAMES, null, DATABASE_VERSION);
+    }
+}
